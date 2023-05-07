4434ae60410ce2b961dea0a40a5a54847b536e0d
efen stakes <efenstakes101@gmail.com>
2023-05-07 12:56:58 +0300

    add scify experience component

---
+++ src/components/scify_experience/X2Drone.jsx
@@ -0,0 +1,94 @@
+import React, { useRef, useEffect, useState } from "react";
+import { useGLTF, useAnimations } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+
+
+
+const SCALE = 2.5
+export default function X2Drone(props) {
+    const group = useRef(null);
+  
+    const { nodes, materials, animations } = useGLTF("/x2.glb");
+    const { actions } = useAnimations(animations, group);
+  
+    const [posZ, setPosZ] = useState(-5)
+  
+    // console.log("actions ", actions)
+  
+    useEffect(() => {
+      // actions.Hover.play()
+    }, [])
+  
+    useFrame(()=> {
+      setPosZ((state)=> {
+        return state < 0 ? state += .01 : state
+      })
+    })
+
+    
+
+  return (
+    <group ref={group} {...props} dispose={null} rotation-y={ -Math.PI / 2 } scale={[ SCALE, SCALE, SCALE ]}>
+      <group name="Scene">
+        <group
+          name="Sketchfab_model"
+          rotation={[-Math.PI / 2, Math.PI / 9, -Math.PI / 2]}
+        >
+          <group
+            name="CYBER-hoverobjcleanermaterialmergergles"
+            position={[-0.9, -0.6, -0.14]}
+          >
+            <group name="Object_2">
+              <mesh
+                name="Object_3"
+                castShadow
+                receiveShadow
+                geometry={nodes.Object_3.geometry}
+                material={materials["Material.067"]}
+              />
+              <mesh
+                name="Object_4"
+                castShadow
+                receiveShadow
+                geometry={nodes.Object_4.geometry}
+                material={materials.Green}
+              />
+              <group name="Object_5">
+                <mesh
+                  name="Object_2_1"
+                  castShadow
+                  receiveShadow
+                  geometry={nodes.Object_2_1.geometry}
+                  material={materials.Blackish}
+                />
+                <mesh
+                  name="Object_2_2"
+                  castShadow
+                  receiveShadow
+                  geometry={nodes.Object_2_2.geometry}
+                  material={materials.Blackish}
+                />
+                <mesh
+                  name="Object_2_3"
+                  castShadow
+                  receiveShadow
+                  geometry={nodes.Object_2_3.geometry}
+                  material={materials.Green}
+                />
+              </group>
+              <mesh
+                name="Object_6"
+                castShadow
+                receiveShadow
+                geometry={nodes.Object_6.geometry}
+                material={materials.Green}
+              />
+            </group>
+          </group>
+        </group>
+      </group>
+    </group>
+  );
+}
+
+useGLTF.preload("/x2.glb");
