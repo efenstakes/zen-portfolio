e4eca90e147c88fec3e031611766ad5504f4dcd7
efen stakes <efenstakes101@gmail.com>
2023-05-07 18:48:49 +0300

    add app models

---
+++ src/models/project.tsx
@@ -0,0 +1,11 @@
+
+
+export interface Project {
+    name?: string
+    img?: string
+    link?: string
+    description?: string
+    img_styles?: any,
+    type?: string
+    tags: Array<string>
+}
